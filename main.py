f4f63bf17bf71510c3f689ae823cfc820650f7eb
Nguyễn Hoàng Anh Dũng <iovn1259@gmail.com>
2024-09-12 22:27:36 +0700

    revert to old version after vercel fail

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import os
-import asyncio
-import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from api.base import api_router
@@ -17,23 +14,14 @@ app.add_middleware(
 )
 
 app.include_router(api_router)
+# app.include_router(api_router, prefix="/api")
 
 
 @app.get("/")
-async def read_root():
+def read_root():
     return {"Hello": "World"}
 
 
-async def main():
-    config = uvicorn.Config("main:app", host=settings.API_HOST, port=settings.API_PORT, reload=True)
-    server = uvicorn.Server(config)
-    
-    # Run uvicorn within asyncio's event loop
-    await server.serve()
-
-
 if __name__ == "__main__":
-    # Check if running locally or in a server environment
-    if os.getenv('VERCEL_ENV') is None:
-        # Run the asyncio main function
-        asyncio.run(main())
+    import uvicorn
+    uvicorn.run("main:app", host=settings.API_HOST, port=settings.API_PORT, reload=True)
